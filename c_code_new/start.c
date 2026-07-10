30fbec9257a852f773c4620538eb60b096f1ca28
EABene <benedikt.sandler@gmail.com>
2026-07-10 19:26:06 +0200

    Continue

--- c_code_new/start.c
+++ c_code_new/start.c
@@ -1,2 +1,20 @@
 #include <unistd.h>
 
+void ft_putst(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        // hier ein Zeichen ausgeben - mit write, so wie bei put_char
+        i++;
+    }
+}
+
+int main(void)
+{
+    ft_putstr("Hallo Test");
+    write(1, "\n", 1);
+    return(0);
+}
