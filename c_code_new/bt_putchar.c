e7085496ac924a0fae74c6f5ff86b2a7ba732d36
EABene <benedikt.sandler@gmail.com>
2026-07-11 15:55:31 +0200

    Renamings, add comments

--- c_code_new/bt_putchar.c
+++ c_code_new/bt_putchar.c
@@ -1,14 +1,14 @@
 #include <unistd.h> // wie import
 
-void ft_putchar(char c) // Funktionsdefinition, void = gibt nichts zurück
+void bt_putchar(char c) // Funktionsdefinition, void = gibt nichts zurück
 {                       // char c = Parameter c ist vom Typ char
     write(1, &c, 1);    // 1 = schreibe auf den Bildschirm, &c = Speicherort von c
 }                       // 1 = Anzahl Zeichen
 
 int main(void)
 {
-    ft_putchar('4');
-    ft_putchar('2');
-    ft_putchar('\n');
+    bt_putchar('4');
+    bt_putchar('2');
+    bt_putchar('\n');
     return(0);
 }
