628ed016e9ce7fb4bc337351c65b58ef82137572
EABene <benedikt.sandler@gmail.com>
2026-07-22 17:50:15 +0200

    Reps

--- c_code_new/bt_last_char.c
+++ c_code_new/bt_last_char.c
@@ -1,24 +1,26 @@
-#include <stdio.h>
+#include <stdio.h>  // 100% self coded
+                    // errors: printf: use %c, not %d; x[i - 1], not x[i] - 1
 
-char bt_last_char(char *str)
+
+char bt_last_char(char *x)
 {
     int i;
     i = 0;
 
-    while(str[i] != '\0')
+    while (x[i] != '\0')
     {
         i++;
     }
 
-    return(str[i - 1]);
-
+    return (x[i - 1]);
 }
 
 
 int main(void)
 {
-    char *text = "Hallo";
+    char *x = "Ahoy";
 
-    printf("Last character: %c\n", bt_last_char(text)); // should print 'o'
+
+    printf("Last character: %c\n", bt_last_char(x));
     return (0);
 }
