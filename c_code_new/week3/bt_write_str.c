f5cc87696919757bc5dcf17b41339eb4943000c2
EABene <benedikt.sandler@gmail.com>
2026-07-29 20:59:54 +0200

    Write String Function

---
+++ c_code_new/week3/bt_write_str.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+int bt_str_len(char *str)
+{
+    int i;
+    i = 0;
+    
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+
+    return(i);
+}
+
+void bt_write_str(char *str)
+{
+    int len;
+    len = bt_str_len(str);
+
+    write(1, str, len);
+}
+
+
+
+int main(void)
+{
+    bt_write_str("Hello everybody");
+    write(1, "\n", 1);
+    return(0);
+}
