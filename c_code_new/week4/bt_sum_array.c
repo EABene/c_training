fb9538288f4b59969f3c2c4058a5a3d901ac9d46
EABene <benedikt.sandler@gmail.com>
2026-08-04 18:50:11 +0200

    Improve file

--- c_code_new/week4/bt_sum_array.c
+++ c_code_new/week4/bt_sum_array.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 
-int bt_sum_array(int *arr)
+int bt_sum_array(int *arr, int length)
 {
     int i;
     int j;
     
     i = 0;
     j = 0;
-    while (i < sizeof(arr))
+    while (i < length)
     {
         j = j + arr[i];
         i++;
-    }  
+    }
     return (j);
 }
 
@@ -20,13 +20,13 @@ int bt_sum_array(int *arr)
 
 int main(void)
 {
-    int x[5] = {1, 2, 3, 4, 5};
+    int  x[5] = {1, 2, 3, 4, 5};
+    int length = 5;
     int y;
 
-    y = bt_sum_array(x);
+    y = bt_sum_array(x, length);
     printf("Sum of array:  %d\n", y);
 
     return (0);
 }
-
 
