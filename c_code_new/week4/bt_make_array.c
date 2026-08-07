8ccd0ac186958b9170234450839a2b474a4e230d
EABene <benedikt.sandler@gmail.com>
2026-08-07 22:21:06 +0200

    code from scratch with vim

--- c_code_new/week4/bt_make_array.c
+++ c_code_new/week4/bt_make_array.c
@@ -1,3 +1,43 @@
 // Zur Erinnerung nur die Aufgabe, keine Lösung: eine Funktion
 // die mit malloc ein Array anlegt, es füllt (z.B. i * 10) und den Pointer zurückgibt.
 // Im main fängst du ihn auf, gibst das Array aus, und gibst es mit free frei.
+#include <stdio.h>
+#include <stdlib.h>
+
+int *bt_make_array(int x)
+{
+    int *arr;
+    int i;
+
+    arr = malloc(sizeof(int) * x);
+
+    i = 0;
+    while (i < x)
+    {
+        arr[i] = (1 +  i) * 10;
+        i++;
+    }
+
+    return (arr);
+}
+
+
+int main(void)
+{
+    int *arr;       // array
+    int i;        // counter
+    int inserts;  // inserts
+
+    inserts = 6;
+    arr = bt_make_array(inserts);
+    i = 0;
+    while (i < inserts)
+    {
+        printf("%d ", arr[i]);
+        i++;
+    }
+    printf("\n");
+    free(arr);
+
+    return (0);
+}
