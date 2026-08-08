727e4692663c0e82e9ceeb181a27ee6102c31804
EABene <benedikt.sandler@gmail.com>
2026-08-08 21:48:15 +0200

    clean up

--- c_code_new/week4/bt_make_array.c
+++ c_code_new/week4/bt_make_array.c
@@ -1,6 +1,3 @@
-// Zur Erinnerung nur die Aufgabe, keine Lösung: eine Funktion
-// die mit malloc ein Array anlegt, es füllt (z.B. i * 10) und den Pointer zurückgibt.
-// Im main fängst du ihn auf, gibst das Array aus, und gibst es mit free frei.
 #include <stdio.h>
 #include <stdlib.h>
 
